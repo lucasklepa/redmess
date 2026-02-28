9f0301a9aacb3828814441b29bf298af72e503dd
lucasklepa <125552754+lucasklepa@users.noreply.github.com>
2026-02-28 12:06:29 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+
+  # Red Mess Website '26
+
+  This is a code bundle for Red Mess Website '26. The original project is available at https://www.figma.com/design/EJ606WCy34HnodXNiyoHh9/Red-Mess-Website--26.
+
+  ## Running the code
+
+  Run `npm i` to install the dependencies.
+
+  Run `npm run dev` to start the development server.
+  
---
+++ src/imports/SpotifyLogoWithText1.tsx
@@ -0,0 +1,18 @@
+import svgPaths from "./svg-d2vcfx903a";
+
+export default function SpotifyLogoWithText() {
+  return (
+    <a className="block cursor-pointer relative size-full" data-name="Spotify_logo_with_text 1" href="https://open.spotify.com/intl-pt/artist/4FfzUSp1S2dTPkkxd9l7l1?si=9SBOS7UqQouxSgW9NTzAYw">
+      <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 104.082 31.2804">
+        <g clipPath="url(#clip0_73_205)" id="Spotify_logo_with_text 1">
+          <path d={svgPaths.p15bed800} fill="var(--fill-0, #F0F0F0)" id="Vector" />
+        </g>
+        <defs>
+          <clipPath id="clip0_73_205">
+            <rect fill="white" height="31.2804" width="104.082" />
+          </clipPath>
+        </defs>
+      </svg>
+    </a>
+  );
+}
